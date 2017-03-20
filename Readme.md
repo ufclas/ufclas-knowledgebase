ca537923e67aa01066b81d7d4f281424a037f15d
priscillamc <priscillamc@users.noreply.github.com>
2017-03-20 18:39:48 -0400

    Initial commit of the files migrated from the theme.

---
+++ Readme.md
@@ -0,0 +1,20 @@
+UFCLAS - Knowledgebase
+======================
+
+Feature plugin for WP Knowlegebase customizations.
+
+Requirements
+------------
+- [WP Knowledgebase](https://wordpress.org/plugins/wp-knowledgebase/)
+
+Installation
+------------
+- Make sure the WP Knowledgebase is installed and activated.
+- Install and activate plugin for a site. (Network Activate hasn't been tested)
+
+Changelog
+---------
+
+### 0.0.0
+
+- Initial commit of the files migrated from the theme.
---
+++ customizer.php
@@ -0,0 +1,83 @@
+<?php 
+/**
+ * Add Knowledgebase settings to the Theme Options in the Customizer
+ *
+ * @since 0.0.0
+ */
+function ufclas_knowledgebase_customize_register( $wp_customize ) {
+	
+	// Newsletter Option
+	$wp_customize->add_section( 'theme_options_kb', array(
+		'title' => __('Knowledge Base', 'ufclas-knowledgebase'),
+		'description' => __('', 'ufclas-knowledgebase'),
+		'panel' => 'theme_options',
+	));
+	
+	$wp_customize->add_setting( 'kb_title', array( 'default' => 'Knowledge Base', 'sanitize_callback' => 'sanitize_text_field' ));
+	$wp_customize->add_control( 'kb_title', array(
+		'label' => __('Knowledge Base Title', 'ufclas-knowledgebase'),
+		'description' => __('Title that appears above the search field and in breadcrumbs', 'ufclas-knowledgebase'),
+		'section' => 'theme_options_kb',
+		'type' => 'text',
+	));
+	
+	$wp_customize->add_setting( 'kb_columns', array( 'default' => 2, 'sanitize_callback' => 'ufclas_ufl_2015_sanitize_choices' ));
+	$wp_customize->add_control( 'kb_columns', array(
+		'label' => __('Columns', 'ufclas-knowledgebase'),
+		'description' => __('Number of columns displayed on the home page', 'ufclas-knowledgebase'),
+		'section' => 'theme_options_kb',
+		'type' => 'select',
+		'choices' => array(
+			1 => '1',
+			2 => '2',
+			3 => '3',
+			4 => '4',
+		),
+	));
+	
+	$wp_customize->add_setting( 'kb_show_count', array( 'default' => 1, 'sanitize_callback' => 'absint' ));
+	$wp_customize->add_control( 'kb_show_count', array(
+		'label' => __('Show Article Count', 'ufclas-knowledgebase'),
+		'description' => __('', 'ufclas-knowledgebase'),
+		'section' => 'theme_options_kb',
+		'type' => 'checkbox',
+	));
+	
+	$wp_customize->add_setting( 'kb_post_order', array( 'default' => 'menu_order', 'sanitize_callback' => 'ufclas_ufl_2015_sanitize_choices' ));
+	$wp_customize->add_control( 'kb_post_order', array(
+		'label' => __('Article Order', 'ufclas-knowledgebase'),
+		'description' => __('Select order for articles', 'ufclas-knowledgebase'),
+		'section' => 'theme_options_kb',
+		'type' => 'select',
+		'choices' => array(
+			'menu_order' => __('Article Sort Order', 'ufclas-knowledgebase'),
+			'title' => __('Article Title', 'ufclas-knowledgebase'),
+		),
+	));
+	
+	$wp_customize->add_setting( 'kb_term_order', array( 'default' => 'terms_order', 'sanitize_callback' => 'ufclas_ufl_2015_sanitize_choices' ));
+	$wp_customize->add_control( 'kb_term_order', array(
+		'label' => __('Category Order', 'ufclas-knowledgebase'),
+		'description' => __('Select order for categories', 'ufclas-knowledgebase'),
+		'section' => 'theme_options_kb',
+		'type' => 'select',
+		'choices' => array(
+			'terms_order' => __('Category Sort Order', 'ufclas-knowledgebase'),
+			'name' => __('Category Title', 'ufclas-knowledgebase'),
+		),
+	));
+	
+	$wp_customize->add_setting( 'kb_post_order', array( 'default' => 'menu_order', 'sanitize_callback' => 'ufclas_ufl_2015_sanitize_choices' ));
+	$wp_customize->add_control( 'kb_post_order', array(
+		'label' => __('Article Order', 'ufclas-knowledgebase'),
+		'description' => __('Select order for articles', 'ufclas-knowledgebase'),
+		'section' => 'theme_options_kb',
+		'type' => 'select',
+		'choices' => array(
+			'menu_order' => __('Article Sort Order', 'ufclas-knowledgebase'),
+			'title' => __('Article Title', 'ufclas-knowledgebase'),
+		),
+	));
+	
+}
+add_action('customize_register','ufclas_knowledgebase_customize_register');
