757d8e2dacaa6ca35f2727568217de8e7363dd3c
priscillamc <priscillamc@users.noreply.github.com>
2017-08-16 21:43:58 -0400

    Fixes issue preventing passing query to UF Search

--- js/kb.js
+++ js/kb.js
@@ -7,12 +7,12 @@ var homeUrl = kb_data.home_url;
 ajax.open("GET", homeUrl + 'wp-json/wp/v2/kb/search', true);
 ajax.onload = function() {
 	var list = JSON.parse(ajax.responseText).map(function(i) { return {label: i.title, value: i.link}; });
-	new Awesomplete(document.querySelector("#s"),{ list: list });
+	new Awesomplete(document.querySelector("#query-kb"),{ list: list });
 };
 ajax.send();
 
 // Send user to the page when autocomplete title is clicked
-document.querySelector("#s").addEventListener('awesomplete-select', function(e){
+document.querySelector("#query-kb").addEventListener('awesomplete-select', function(e){
 	e.preventDefault();
 	var suggestion = e.text;
 	window.location = suggestion.value;
